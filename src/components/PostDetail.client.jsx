b0dfb0a17769d41a1705c3527ef9d74bbccd2aae
JSer <69352453+JSerZANP@users.noreply.github.com>
2023-04-09 21:49:27 +0900

    split PostDetail into client part and server part

---
+++ src/components/PostDetail.client.jsx
@@ -0,0 +1,56 @@
+const createPostFetcher = function (permalink) {
+  return {
+    data: null,
+    promise: null,
+    fetch(id) {
+      if (this.data != null) {
+        return this.data;
+      }
+
+      if (this.promise == null) {
+        const payload = {
+          component: "PostDetail",
+          props: {
+            permalink,
+          },
+        };
+
+        // pa
+        this.promise = fetch("/render", {
+          body: JSON.stringify(payload),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => res.text())
+          .then((str) => {
+            this.data = JSON.parse(str, (key, value) => {
+              if (key === "$$typeof") {
+                if (value === "Symbol(react.element)") {
+                  return Symbol.for("react.element");
+                }
+                throw new Error("unexpected $$typeof", value);
+              }
+              return value;
+            });
+          });
+      }
+
+      throw this.promise;
+    },
+  };
+};
+
+const map = new Map();
+
+const Post = (permalink) => {
+  if (!map.has(permalink)) {
+    map.set(permalink, createPostFetcher(permalink));
+  }
+  return map.get(permalink);
+};
+
+export default function PostDetail({ permalink }) {
+  return Post(permalink).fetch();
+}
---
+++ src/components/PostDetail.server.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { marked } from "marked";
+import { getPosts } from "../server/posts";
+
+export default async function PostView({ permalink }) {
+  const list = await getPosts();
+  const post = list.filter((post) => post.permalink === permalink)[0];
+  const html = marked(post.content);
+
+  return <p dangerouslySetInnerHTML={{ __html: html }}></p>;
+}
