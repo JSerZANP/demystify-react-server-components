80d52b51f795b786bd2d70ee488363005ec4ad9e
JSer <69352453+JSerZANP@users.noreply.github.com>
2023-04-09 21:51:02 +0900

    add endpoint /render

--- src/server/index.js
+++ src/server/index.js
@@ -13,6 +13,26 @@ app.get("/api/posts", async (req, res) => {
   const list = await getPosts();
   res.json(list);
 });
+
+app.post("/render", async (req, res) => {
+  const { component, props } = req.body;
+  const Component = require(path.join(
+    __dirname,
+    "../components/" + component + ".server.js"
+  )).default;
+
+  // assume all server components are async for now
+  const json = await Component(props);
+  const str = JSON.stringify(json, (k, v) => {
+    if (k === "$$typeof" && typeof v === "symbol") {
+      return v.toString();
+    }
+    return v;
+  });
+
+  res.send(str);
+});
+
 app.get("/api/post/:permalink", async (req, res) => {
   const list = await getPosts();
   res.json(list.filter((post) => post.permalink === req.params.permalink)[0]);
